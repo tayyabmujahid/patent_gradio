4d6960ceff34bf855368b06f119baa3ce2247c8c
TM_workstation <tayyab.mujahid@gmail.com>
2022-09-14 16:17:20 +0300

    add key word extractor...in progress

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ from pathlib import Path
 import gradio as gr
 import logging
 
+from keywod_extractor import KeyWordExtractor
+
 os.environ["CUDA_VISIBLE_DEVICES"] = ""
 logging.basicConfig(level=logging.INFO)
 APP_DIR = Path(__file__).resolve().parent  # what is the directory for this application?
@@ -14,7 +16,12 @@ EG_PATH = os.path.join(APP_DIR, "examples")
 
 
 def main():
-    frontend = make_frontend(fn=get_patent_abstract)
+    # close all previous sessions
+    gr.close_all()
+    extractor = ExtractorBackend()
+    frontend = make_frontend(
+        fn=extractor.run
+    )
     frontend.launch(
         server_name="0.0.0.0",  # make server accessible, binding all interfaces  # noqa: S104
         server_port=8000,  # set a port to bind to, failing if unavailable
@@ -25,27 +32,54 @@ def main():
 
 def get_patent_abstract():
     text = 'Test Abstract'
+
     return text
 
 
-def make_frontend(fn: Callable[[Image], str]):
+place_holder_text = 'A method for preparing a polygraphene membrane includes adding ' \
+                  'graphite and sodium nitrate into sulfuric acid to form a first mixture; ' \
+                  'adding potassium permanganate solution into the first mixture to form a ' \
+                  'second mixture;adding hydrogen peroxide solution to the second mixture to ' \
+                  'form a mixture including soluble manganese ions; filtering the mixture including ' \
+                  'soluble manganese ions to form an aqueous suspension; centrifuging the aqueous suspension; ' \
+                  'performing ultrasonication of the suspension to obtain graphene oxide sheets; ' \
+                  'acylating the graphene oxide sheets to prepare an acylated graphene oxide sheet;' \
+                  ' and polymerizing the acylated graphene oxide sheets to prepare polygraphene.'
+
+
+def make_frontend(fn: Callable[[str], list]):
     allow_flagging = "never"
     readme = _load_readme(with_logging=allow_flagging == "manual")
     frontend = gr.Interface(
         fn=fn,  # which Python function are we interacting with?
         outputs=gr.components.Textbox(),  # what output widgets does it need? the default text widget
         # what input widgets does it need? we configure an image widget
-        inputs=gr.components.Textbox(),
+        inputs=gr.components.Textbox(placeholder=_place_holder_input_text()),
         title="📝 Key Word Extraction",  # what should we display at the top of the page?
 
         description=__doc__,  # what should we display just above the interface?
         article=readme,  # what long-form content should we display below the interface?
-        examples=_examples(),  # which potential inputs should we provide?
+        examples=place_holder_text,  # which potential inputs should we provide?
         cache_examples=False,  # should we cache those inputs for faster inference? slows down start
         # allow_flagging=allow_flagging,  # should we show users the option to "flag" outputs?
     )
 
     return frontend
+
+
+def _place_holder_input_text():
+    place_holder_text = 'A method for preparing a polygraphene membrane includes adding ' \
+                        'graphite and sodium nitrate into sulfuric acid to form a first mixture; ' \
+                        'adding potassium permanganate solution into the first mixture to form a ' \
+                        'second mixture;adding hydrogen peroxide solution to the second mixture to ' \
+                        'form a mixture including soluble manganese ions; filtering the mixture including ' \
+                        'soluble manganese ions to form an aqueous suspension; centrifuging the aqueous suspension; ' \
+                        'performing ultrasonication of the suspension to obtain graphene oxide sheets; ' \
+                        'acylating the graphene oxide sheets to prepare an acylated graphene oxide sheet;' \
+                        ' and polymerizing the acylated graphene oxide sheets to prepare polygraphene.'
+    return place_holder_text
+
+
 def _load_readme(with_logging=False):
     with open(README) as f:
         lines = f.readlines()
@@ -56,12 +90,35 @@ def _load_readme(with_logging=False):
     return readme
 
 
+def _abstract_example():
+    return _place_holder_input_text()
+
+
 def _examples():
     examples = os.listdir(EG_PATH)
-    print(examples)
-
     examples = [os.path.join(EG_PATH, i) for i in examples]
     return examples
+
+
+class ExtractorBackend:
+    def __init__(self):
+        model = KeyWordExtractor()
+        self._predict = model.predict
+
+    def run(self, doc):
+        preds, metrics = self._extract_with_metrics(doc)
+
+        return preds
+
+    def _extract_with_metrics(self, doc):
+        keywords = list()
+        relevances = list()
+        preds = self._predict(doc)
+
+        for word, metric in preds:
+            keywords.append(word)
+            relevances.append(metric)
+        return keywords, relevances
 
 
 if __name__ == "__main__":
---
+++ keywod_extractor.py
@@ -0,0 +1,19 @@
+from keybert import KeyBERT
+
+
+class KeyWordExtractor:
+    """Extracts Key word from text"""
+
+    def __init__(self, top_n=5):
+        self.model = KeyBERT()
+        self.top_n = top_n
+
+    def predict(self, doc):
+        keywords = self.model.extract_keywords(docs=doc,
+                                               top_n=self.top_n,
+                                               stop_words='english',
+                                               keyphrase_ngram_range=(1, 2),
+                                               use_mmr=True, diversity=0.7)
+        return keywords
+
+
